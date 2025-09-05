c612d1a50d795d1489b1bef44ca45e25776417d5
Oliver Spain <oliverjspain@gmail.com>
2025-09-05 14:28:36 -0700

    setup update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='wget-archiver',
     version='0.1.0',
-    py_modules=['wget-archiver'],
+    py_modules=['wget_archiver'],
     install_requires=[
         'requests>=2.25.1',
         'beautifulsoup4>=4.9.3',
@@ -18,7 +18,7 @@ setup(
     description='A general-purpose web scraper for archives.',
     long_description='A script that scrapes a given URL, either by following "next" links or by iterating through numbered pages, to collect all article URLs.',
     long_description_content_type='text/plain',
-    url='https://github.com/PLACEHOLDER',
+    url='https://github.com/Ojspain/wget-archiver',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
