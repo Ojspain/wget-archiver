35a5fdfa8ed22eb16a82e510f76c85c8c3a25666
Oliver Spain <oliverjspain@gmail.com>
2025-09-05 14:22:09 -0700

    changes to setup

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='archive-scraper',
-    version='1.0.0',
-    py_modules=['link_grabber'],
+    name='wget-archiver',
+    version='0.1.0',
+    py_modules=['wget-archiver'],
     install_requires=[
         'requests>=2.25.1',
         'beautifulsoup4>=4.9.3',
     ],
     entry_points={
         'console_scripts': [
-            'archive-scraper = link_grabber:main',
+            'wget-archiver = wget_archiver:main',
         ],
     },
     author='Oliver Spain',
     author_email='oliverjspain@gmail.com',
     description='A general-purpose web scraper for archives.',
-    long_description='A script that scrapes a given URL, either by following "next" links or by iterating through numbered pages, to collect all article URLs. It can also download the pages using wget.',
+    long_description='A script that scrapes a given URL, either by following "next" links or by iterating through numbered pages, to collect all article URLs.',
     long_description_content_type='text/plain',
     url='https://github.com/PLACEHOLDER',
     classifiers=[
